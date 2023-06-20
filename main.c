197bb6c670c480c23e68c37e734d8291f72a50de
Gabriel Alves <alvesg2003@gmail.com>
2023-06-19 22:55:11 -0300

    adicionando transacao e clientes

--- main.c
+++ main.c
@@ -48,6 +48,10 @@ typedef struct transacao{
     
 } Transacao;
 
+Cliente cliente_nulo={"~","000.000.000-00",{0,0},{0,0,0}};
+Cliente clientes[100];
+int numero_de_clientes=0;
+
 int validar_data(Data a){
     if(1<=a.dia&&a.dia<=30){
         if(1<=a.mes&&a.mes<=12){
@@ -76,46 +80,40 @@ Data criar_data(void){
 }
 
 int validar_cpf(char cpf[15]){
-    int soma = 0;
-    int resto = 0;
-
-    for(int c = 0; c < 9; c++){
-
-        soma += ((cpf[c] - 48) * (10-c));
+    int ncpf[11]={0,0,0,0,0,0,0,0,0,0,0};
+    int num=0;
+    for(int i=0; i<3; i++){
+        ncpf[i]=cpf[i]-48;
     }
-    resto = soma % 11;
-
-    if (resto < 2){     
-        if(cpf[10]-48 != 0){
+    for(int i=4; i<7; i++){
+        ncpf[i-1]=cpf[i]-48;
+    }
+    for(int i=8; i<11; i++){
+        ncpf[i-2]=cpf[i]-48;
+    }
+    for (int i=0; i<9; i++){
+        num+=(ncpf[i])*(i+1);
+    }
+    num=num%11;
+    if(num<2){
+        num=0;
+    }
+    if(num!=cpf[12]-48){
+        return 0;
+    } else{
+        ncpf[9]=num;
+        num=0;
+        for (int i=0; i<9; i++){
+            num+=(ncpf[i+1])*(10-i);
+        }
+        num=num%11;
+        num=11-num;
+        if(num!=cpf[13]-48){
             return 0;
-        }
-
-    }else{
-        if(cpf[10]-48 != 11-resto){
-            return 0;   
+        }else{
+            return 1;
         }
     }
-
-    soma = (cpf[10] - 48) * 2;
-
-    for(int c = 0; c < 9; c++){
-
-        soma += ((cpf[c] - 48) * (11-c));
-    }
-
-    resto = soma % 11;
-
-    if (resto < 2){     
-        if(cpf[11] != 0){
-            return 0;
-        }
-
-    }else{
-        if(cpf[11]-48 != 11-resto){
-            return 0;   
-        }
-    }
-    return 1;
 }
 
 int validar_telefone(Telefone a){
@@ -128,10 +126,195 @@ int validar_telefone(Telefone a){
     return 0;
 }
 
+Investimento cadastrar_invenstimento(void){
+    Investimento investimento;
+    
+    getchar();
+    printf("Banco emissor: ");
+    gets(investimento.emissor);
+    printf("Qual o tipo de aplicação (1 / 2 / 3): ");
+    scanf("%d", &investimento.tipoAplicacao);
+    printf("Qual a porcentagem da taxa sobre o investimento: ");
+    scanf("%f", &investimento.taxa);
+    investimento.ativo = 'S';
+    printf("\n\n");
+    return investimento;
+}
+
+Transacao comprar_investimento(int id, Cliente c, Investimento i){
+    Transacao transacao;
+    transacao.idTransacao = id;
+    transacao.cliente = c;
+    transacao.investimento = i;
+    transacao.dataAplicacao = {19, 06, 2023};
+    printf("quanto gostaria de investir nessa ação: ");
+    scanf("%f", &transacao.valorAplicacao);
+    transacao.dataResgate = {0, 0, 0};
+    transacao.valorResgate = 0.0;
+    return transacao;
+}
+
+void Criar_cliente (void){
+    for(int p=0; p<1; p++){
+        Cliente b;
+        Telefone t;
+        char Cpf[15];
+        printf("Nome do Cliente.: ");
+        getchar();
+        fgets(b.nome, 50, stdin);
+        b.nome[strlen(b.nome)-1]='\0';
+        char cpf_formatado[15];
+        int ver=0;
+        while(ver==0){
+            printf("Digitar CPF do Cliente (ex: xxx.xxx.xxx-xx).: ");
+            fgets(Cpf, 15, stdin);
+            for(int i=0; i<3; i++){
+                cpf_formatado[i]=Cpf[i];
+            }
+            for(int i=4; i<7; i++){
+                cpf_formatado[i]=Cpf[i];
+            }
+            for(int i=8; i<11; i++){
+                cpf_formatado[i]=Cpf[i];
+            }
+            for(int i=12; i<14; i++){
+                cpf_formatado[i]=Cpf[i];
+            }
+            cpf_formatado[3]='.';
+            cpf_formatado[7]='.';
+            cpf_formatado[11]='-';
+            
+            ver=validar_cpf(cpf_formatado);
+            if(ver==0){
+                printf("CPF inválido!\n");
+                getchar();
+            }
+        }
+        int cpf_repetido=0;
+        for(int q=0; q<numero_de_clientes; q++){
+            if(strcmp(clientes[q].CPF, cpf_formatado)==0){
+                cpf_repetido=1;
+            }
+        }
+        if(cpf_repetido==0){
+            strcpy(b.CPF, cpf_formatado);
+            printf("Digitar o Telefone do Cliente (ex: xx xxxxxxxx).: ");
+            scanf("%d%ld", &t.DDD ,&t.numero);
+            while(validar_telefone(t)==0){
+                printf("Número Inválido!\nDigite novamente.: ");
+                scanf("%d%ld", &t.DDD, &t.numero);
+            }
+            b.fone=t;
+            printf("Digitar data do nascimento do Cliente (ex: dd mm yyyy)\n");
+            Data d;
+            do{
+                d=criar_data();
+                if(validar_data(d)==0){
+                    printf("Data inválida\n");
+                }
+            }while(validar_data(d)==0);
+            b.nascimento=d;
+            clientes[numero_de_clientes]=b;
+            numero_de_clientes++;
+        }else{
+            printf("Já existe um cliente cadastrado com esse CPF, cadastro cancelado\n");
+        }
+    }
+    printf("\n");
+}
+
+void exibir_clientes(){
+    for(int i=0; i<numero_de_clientes; i++){
+        printf("Nome.: %s | Telefone.: %d %ld | CPF.: %s | ", clientes[i].nome, clientes[i].fone.DDD, clientes[i].fone.numero, clientes[i].CPF);
+        exibir_data(clientes[i].nascimento);
+    }
+}
+
+void alfabetica(Cliente a[]){
+    Cliente aux={};
+    for (int j=0; j<100; j++){
+        for (int i=0; i<98; i++){
+            if((a[i+1].nome[0]>65)&&(a[i].nome[0]>65)&&strcmp(a[i].nome, a[i+1].nome)>0){
+                aux=a[i+1];
+                a[i+1]=a[i];
+                a[i]=aux;
+            }
+        }
+    }
+}
+
+void excluir_cliente(char cpf[]){
+    char cpf_formatado[15];
+    for(int i=0; i<3; i++){
+        cpf_formatado[i]=cpf[i];
+    }
+    for(int i=4; i<7; i++){
+        cpf_formatado[i]=cpf[i];
+    }
+    for(int i=8; i<11; i++){
+        cpf_formatado[i]=cpf[i];
+    }
+    for(int i=12; i<14; i++){
+        cpf_formatado[i]=cpf[i];
+    }
+    cpf_formatado[3]='.';
+    cpf_formatado[7]='.';
+    cpf_formatado[11]='-';
+    
+    if (validar_cpf(cpf_formatado)==0){
+        printf("CPF inválido");
+    } 
+    
+    for(int q=0; q<numero_de_clientes; q++){
+        if(strcmp(clientes[q].CPF, cpf_formatado)==0){
+            clientes[q]=cliente_nulo;
+            numero_de_clientes--;
+        }
+    }
+}
+
 int main()
 {
-    Telefone fone = {11, 912345678};
-    printf("%d", validar_telefone(fone));
+    Investimento investimentos[30];
+    int count = 0, texto, id_transacao = 0;
+
+    
+    while(1){
+        alfabetica(clientes);
+        printf("Digite\n1 para cadastrar um novo cliente.\n2 para exibir todos os clientes.\n3 para excluir um cliente (pelo CPF).\n4 para criar investimentos\n5 Compra Investimentos\n\n");
+        scanf("%d", &texto);
+        if (texto==1){
+           Criar_cliente();
+        }
+        if (texto==2){
+           exibir_clientes();
+           printf("\n");
+        }
+        if (texto==3){
+            char cpf_a_ser_excluido[15];
+            getchar();
+            fgets(cpf_a_ser_excluido, 15, stdin);
+            excluir_cliente(cpf_a_ser_excluido);
+            printf("\n\n");
+       }
+       if(texto == 4){
+        do {
+            printf("quantos investimentos gostaria de realizar? ");
+            scanf("%d", &count);
+        } while (count < 1 || count > 30);
+
+        for(int i = 0; i < count; i++){
+            investimentos[i] = cadastrar_invenstimento();
+        }
+       }
+       if(texto == 5){
+            
+            // printf("");
+
+            // comprar_investimento(id_transacao, );
+            // id_transacao++;
+       }
+   }
 
     return 0;
 }
